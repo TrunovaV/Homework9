83f0e4b4caeb21ba53548207629e5a9149241df9
Viktoria <viki.panterka@yandex.ru>
2026-07-10 21:56:03 +0300

    Исправления

--- src/Main.java
+++ src/Main.java
@@ -27,8 +27,8 @@ public class Main {
         }
         float averageSum = (float) sum / inputArray1.length;
         outputArray1 [0] = sum;
-        outputArray1 [1] = minSum;
-        outputArray1 [2] = maxSum;
+        outputArray1 [1] = maxSum;
+        outputArray1 [2] = minSum;
         outputArray1 [3] = averageSum;
         System.out.println(Arrays.toString(inputArray1));
         System.out.println(Arrays.toString(outputArray1));
@@ -49,7 +49,7 @@ public class Main {
         boolean [] outputArray3 = new boolean[5];
         int i = 0;
         for (int bonus : inputArray3) {
-            outputArray3 [i] = bonus >= 5000;
+            outputArray3 [i] = bonus > 5000;
             i++;
         }
         System.out.println(Arrays.toString(inputArray3));
@@ -58,31 +58,32 @@ public class Main {
         System.out.println("Задача 4");
 
         int [] inputArray4 = {3000, 1000, 500, -600, 1000};
-        boolean outputArray4 = true;
+        boolean [] outputArray4 = new boolean[1];
         int index = 0;
 
         for (int remainder : inputArray4) {
-            outputArray4 = remainder > 0;
+            outputArray4 [0] = remainder > 0;
             index++;
 
             if (remainder < 0) {
-                outputArray4 = false;
+                outputArray4 [0] = false;
                 break;
             }
         }
         System.out.println(Arrays.toString(inputArray4));
-        System.out.println(outputArray4);
+        System.out.println(outputArray4 [0]);
 
         System.out.println("Задача 5");
 
         int [] inputArray5 = {40_000, 55_000, -20_000, 60_000, -70_000, };
-        int outputArray5 = 0;
+        int [] outputArray5 = new int [1];
+        outputArray5 [0] = 0;
         for (int profit : inputArray5) {
             if (profit > 0) {
-                outputArray5++;
+                outputArray5 [0]++;
             }
         }
         System.out.println(Arrays.toString(inputArray5));
-        System.out.println(outputArray5);
+        System.out.println(outputArray5[0]);
     }
 }
